87c7e04fe7873c243fab137fc19cc4e37822881e
a-nady <17anady@gmail.com>
2021-06-07 20:41:18 -0400

    add remove usernames, remove hashtags options, add more exceptions

--- user_gen.py
+++ user_gen.py
@@ -1,10 +1,22 @@
+'''
+    TODO:
+        - Somehow add apostrophes between the two words
+        - Implement new generator with NLTK
+'''
+
+
 import sentence_generator as sg
 import get_tweets as gt
 import sys
 
 MARKOV_LENGTH = 2
+
 # higher limit = more unique sentences but slower, lower limit = less unique but faster
 LIMIT = 500
+
+# 1 = to remove, 0 = not to remove
+REMOVE_USERNAMES = 1
+REMOVE_HASHTAGS = 1
 
 def main():
     while 1:
@@ -14,14 +26,14 @@ def main():
             gt.download_tweets(username, LIMIT)
             break
         except ValueError:
-            print("User not found")
-    
+            print("User not found or user is private.")
+
     while 1:
         print()
         try:
-            sg.generate(username, MARKOV_LENGTH)
+            sg.generate(username, MARKOV_LENGTH, REMOVE_USERNAMES, REMOVE_HASHTAGS)
         # will happen when sentence generator attempts to pop off words an empty list, i.e. not enough tokens
-        except IndexError:
+        except (IndexError, FileNotFoundError):
             print("Not enough tweets/words from user to generate a unique output.")
             sys.exit()
 
